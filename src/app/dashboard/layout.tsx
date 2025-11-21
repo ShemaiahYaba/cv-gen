148da40dc4fff854bc00b5ba75bff4dad2f02154
Shemaiah Yaba-Shiaka <apostleshem17@gmail.com>
2025-11-21 14:44:04 +0000

    move the logout button to the header (_for element <LinkComponent>_)

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -7,12 +7,6 @@ import {
 import { Form2CVLogo } from "@/components/icons"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Button } from "@/components/ui/button"
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger,
-  } from "@/components/ui/dropdown-menu"
 
 export default function DashboardLayout({
   children,
@@ -33,24 +27,18 @@ export default function DashboardLayout({
                             user@email.com
                         </p>
                     </div>
-                    <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                            <Button variant="ghost" className="relative h-10 w-10 rounded-full">
-                                <Avatar>
-                                    <AvatarImage src="https://picsum.photos/seed/1/40/40" alt="User avatar" data-ai-hint="person face" />
-                                    <AvatarFallback>U</AvatarFallback>
-                                </Avatar>
-                            </Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent className="w-56" align="end" forceMount>
-                            <DropdownMenuItem asChild>
-                                 <Link href="/auth/sign-in">
-                                    <LogOut className="mr-2 h-4 w-4" />
-                                    <span>Log out</span>
-                                </Link>
-                            </DropdownMenuItem>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
+                    <Button variant="ghost" className="relative h-10 w-10 rounded-full">
+                        <Avatar>
+                            <AvatarImage src="https://picsum.photos/seed/1/40/40" alt="User avatar" data-ai-hint="person face" />
+                            <AvatarFallback>U</AvatarFallback>
+                        </Avatar>
+                    </Button>
+                    <Button variant="outline" size="sm" asChild>
+                        <Link href="/auth/sign-in">
+                            <LogOut className="mr-2 h-4 w-4" />
+                            <span>Log out</span>
+                        </Link>
+                    </Button>
                 </div>
             </header>
             <main className="flex-1 flex-col bg-background p-4 md:p-8">
